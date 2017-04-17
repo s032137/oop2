2ab3b97e1c3a957e5b7a5327e5007163b5912ed7
yinktam0625 <yinktam0625@icloud.com>
2017-04-18 02:49:16 +0800

    GUI update

--- Gui.java
+++ Gui.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import java.awt.*;
 import java.awt.event.*;
@@ -106,3 +107,15 @@ public class Gui {
          
      } 
 }
+=======
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.io.*;
+import javax.swing.border.*;
+
+
+public class Gui extends JFrame{
+
+>>>>>>> origin/master
--- TestSystem.java
+++ TestSystem.java
@@ -1,11 +1,14 @@
-
 public class TestSystem {
+<<<<<<< HEAD
    public static void main(String args[]){
        
        
        
        
        
+=======
+   public static void main(String args[]){      
+>>>>>>> origin/master
        Employee Man1 = new Manager("M000003", "CHIU Shun Kin", 33, "Male", "95515368", "HR Manager", "Room B,27/F,234 Chu Wing Street,Hung Hom,Kowloon", 115000, 5750, 11500, 11500);
 
        Employee Gen1 = new GeneralStaff("G000004", "AU Siu Ling", 26, "Female", "NA", "Technician", "Flat D,10/F,185 Kowloon City Road,Hung Hom,Kowloon", 
@@ -18,6 +21,7 @@ public class TestSystem {
        
        Gui basicUI = new Gui();
        basicUI.contentObjects();
+       
        
        
        
